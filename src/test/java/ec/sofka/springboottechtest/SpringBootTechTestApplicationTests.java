8c86b9af34e4df05468407aac6d3032e2858bb89
pedroortizsolis <portiz@territoriosinteligentes.net>
2025-03-05 18:04:35 -0500

    first commit

---
+++ src/test/java/ec/sofka/springboottechtest/SpringBootTechTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.sofka.springboottechtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTechTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
